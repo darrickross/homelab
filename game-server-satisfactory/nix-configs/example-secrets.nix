2bdc1de56f35bc768686aaf36e4223881ee02ae9
Darrick Ross <me@darrickross.com>
2025-04-09 22:29:02 -0400

    Enable `nixfmt`

--- game-server-satisfactory/nix-configs/example-secrets.nix
+++ game-server-satisfactory/nix-configs/example-secrets.nix
@@ -1,15 +1,13 @@
 {
-    # System Networking config
-    system-hostname         = "satisfactory-prod";
-    system-ipv4-address     = "192.168.0.41";       # Replace with your static IP address
-    system-ipv4-prefix      = 24;
-    system-ipv4-gateway     = "192.168.0.1";        # Replace with your gateway IP address
-    system-ipv4-dns-list    = [
-        "192.168.0.4"
-        "192.168.0.1"
-    ];
-    system-ssh-port         = "22";
+  # System Networking config
+  system-hostname = "satisfactory-prod";
+  system-ipv4-address = "192.168.0.41"; # Replace with your static IP address
+  system-ipv4-prefix = 24;
+  system-ipv4-gateway = "192.168.0.1"; # Replace with your gateway IP address
+  system-ipv4-dns-list = [ "192.168.0.4" "192.168.0.1" ];
 
-    # Satisfactory Ports
-    server-game-port        = "7777";
+  system-ssh-port = "22";
+
+  # Satisfactory Ports
+  server-game-port = "7777";
 }
